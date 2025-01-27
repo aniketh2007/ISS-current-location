23c393cb7e18dc6f82fd974d6190ab6d4966d152
Aniketh H <61650767+aniketh2007@users.noreply.github.com>
2025-01-27 20:28:16 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from datetime import datetime
 import smtplib
 import time
 
-MY_EMAIL = "abc@gmail.com"
-MY_PASSWORD = "Abc"
-MY_LAT = 12.9716  # Your latitude
-MY_LONG = 77.5946  # Your longitude
-
+username = "anikethh08@gmail.com"
+password = "avyaqiilkyixtrui"
+MY_LAT = 51.507351 # Your latitude
+MY_LONG = -0.127758 # Your longitude
 
 def is_iss_overhead():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
@@ -17,10 +16,10 @@ def is_iss_overhead():
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
 
-    # Your position is within +5 or -5 degrees of the ISS position.
-    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG + 5 <= iss_longitude <= MY_LONG + 5:
-        return True
-
+    #Your position is within +5 or -5 degrees of the iss position.
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True 
+    
 
 def is_night():
     parameters = {
@@ -28,7 +27,6 @@ def is_night():
         "lng": MY_LONG,
         "formatted": 0,
     }
-
     response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
     response.raise_for_status()
     data = response.json()
@@ -39,14 +37,18 @@ def is_night():
 
     if time_now >= sunset or time_now <= sunrise:
         return True
-
-
-while True:
+    
+while True: 
     time.sleep(60)
     if is_iss_overhead() and is_night():
-        connection = smtplib.SMTP("smtp.gmail.com")
-        connection.starttls()
-        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-        connection.sendmail(from_addr=MY_EMAIL,
-                            to_addrs="xyz7@gmail.com",
-                            msg=f"Subject: Look Up 👆🏻!\n\n The ISS is above you in the sky")
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=username,password=password)
+            connection.sendmail(from_addr=username,to_addrs="anikethh7@gmail.com",
+                                msg="Subject:Look UP 👆!!!\n\n"
+                                    "The ISS is over your head look up and watch it")
+
+
+
+
+
